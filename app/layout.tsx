003fe2eddbddc95da72b662edeb4fb204127be80
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2023-12-26 12:41:45 +0100

    complete anime app project

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+
+import Hero from "@/components/Hero";
+import Footer from "@/components/Footer";
+
+import "./globals.css";
+
+const dmSans = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Anime Vault",
+  description: "Your favorite anime, all in one place.",
+  keywords:"made in lagos, all you favorite anime's in one place",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={dmSans.className}>
+        <main className="max-w-7xl mx-auto bg-[#0F1117]">
+          <Hero />
+          {children}
+          <Footer />
+        </main>
+      </body>
+    </html>
+  );
+}
